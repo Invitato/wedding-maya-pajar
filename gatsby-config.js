23272c6d7dd8b6fe201db730d9e757b99d4b012c
Indra Kusuma <indrakusuma.udinus@gmail.com>
2020-12-09 14:42:49 +0700

    chore: update path location

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* Your site config here */
-  pathPrefix: `/wedding-dila-agin`,
+  pathPrefix: `/wedding-maya-pajar`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-eslint',
